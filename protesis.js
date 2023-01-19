c6c7afefbedde26e77dd35ccebd27230f95b1ae2
Claudia Garcia <claudiacoderhouse@gmail.com>
2023-01-18 22:24:22 -0300

    subiendo la entrega dospara mañana

---
+++ protesis.js
@@ -0,0 +1,25 @@
+class protesis{
+    constructor( nombre, marca, material, precio){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.material = material;
+        this.precio = precio
+    }
+}
+const protesis1 = new protesis("bota","walker", "acrilico", 35000)
+const protesis2 = new protesis("baston", "silfab", "aluminio", 32000)
+const protesis3 = new protesis("muleta", "dema", "aluminio", 10000)
+
+
+console.log(protesis1);
+console.log(protesis2);
+console.log(protesis3);
+
+
+let arrays = [protesis1, protesis2, protesis3]
+
+arrays.forEach((array)=>{
+    console.log(array.nombre);
+    console.log(array.precio);
+});
+
